d6d05782c228bfc9cf3ca678cb7f8e6c199c2c8c
Koder <6042@ait.nsw.edu.au>
2017-08-08 08:09:46 +0000

    test branch2

---
+++ userHomePage.php
@@ -0,0 +1,66 @@
+<?php
+include("includes/database.php");
+session_start();
+
+if(!isset($_SESSION['username']) || !isset($_SESSION['password'])){
+    header("Location: userLogout.php");
+}
+else{
+    $username = $_SESSION['username'];
+}
+
+if(isset($_POST['finished'])){
+    echo "finished is true";
+    $sqlupdate = "UPDATE QUEUE SET STATUS = 'finished' WHERE QUEUE_ID=(SELECT queue_id FROM users WHERE username='$username') AND STATUS='active'";
+    $result = $connection->query($sqlupdate);
+}
+
+//Non join query >> "SELECT ID FROM QUEUE WHERE QUEUE_ID=(SELECT queue_id FROM users WHERE username='$username') AND STATUS = 'waiting' LIMIT 1";
+$sqlquery = "SELECT QUEUE.ID, QUEUE.QUEUE_ID FROM QUEUE INNER JOIN users ON QUEUE.QUEUE_ID = users.queue_id WHERE users.username = '$username' AND (QUEUE.STATUS = 'waiting' OR QUEUE.STATUS='active') LIMIT 1;";
+$result = $connection->query($sqlquery);
+
+if($result->num_rows>0){
+    echo "HELLO";
+    $row = mysqli_fetch_assoc($result);
+    $currenticketID = $row['ID'];
+    $currentqueueID = $row['QUEUE_ID'];
+    $connection->query("UPDATE QUEUE SET STATUS = 'active' WHERE ID='$currenticketID'");
+}else{
+    $currenticketID = "Queue is empty";
+}
+
+mysqli_close($connection);
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <?php include("includes/head.php");?>
+    <body>
+        <div class="header">
+            <div class="logo">
+                <img src="images/logo.png"></img>
+            </div>
+            <div class="nav">
+                <ul>
+                    <li><a href="userHomePage.php">Home</a></li>
+                    <li><a href="userlogout.php">My Queue</a></li>
+                    <li><a href="userlogout.php">HelpHelpHelp</a></li>
+                    <li><a href="userLogout.php">Logout</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="content">
+            <center><b>Current Ticket: </b><?php echo $currenticketID, $currentqueueID;?></center>
+            <div class="flexcontainer">
+                <div class="buttoncontainer">
+                    <form method="POST" class="buttonflex">
+                        <button class="btn btn-primary btn-block" type="submit" name="finished" value="">Finished</button>
+                    </form>
+                    <form method="POST"class="buttonflex">
+                        <button class="btn btn-primary btn-block" type="submit">Next</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ userLogin.php
@@ -0,0 +1,71 @@
+<?php
+include("includes/database.php");
+
+session_start();
+
+if(isset($_POST['user'])){
+    $user = $_POST['user'];
+    $password = $_POST['password'];
+    
+    $sqllogin = "SELECT * FROM users WHERE username='$user'";
+    
+    $result = $connection->query($sqllogin);
+    if($result->num_rows>0){
+        $userdata = $result->fetch_assoc();
+        //check if password matches the one in the database
+        $dbusername = $userdata['username'];
+        $dbpassword = $userdata['password'];
+        
+        if($dbpassword==$password){
+            $_SESSION['username'] = $user;
+            $_SESSION['password'] = $password;
+            header("Location: userHomePage.php");
+        }else{
+            $error = "Username or password is incorrect";
+        }
+    }else{
+        $error = "Username or password is incorrect";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <?php $page_title = "Login"; include("includes/head.php");?>
+    <body>
+        <div class="header">
+            <div class="logo">
+                <img src="images/logo.png"></img>
+            </div>
+        </div>
+        <div class="container">
+            <div clas="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <h2>Login into your account</h2>
+                    <form id="register-form" method="post">
+                        <?php 
+                        if($error){
+                          $errorclass = "has-error";  
+                        }
+                        ?>
+                        <div class="form-group <?php echo $errorclass; ?>">
+                            <label for="user">Username</label>
+                            <input class="form-control" type="text" id="user" placeholder="Username" name="user">
+                        </div>
+                        <div class="form-group <?php echo $errorclass; ?>">
+                            <label for="password">Password</label>
+                            <input class="form-control" type="password" id="password" placeholder="Password" name="password">
+                        </div>
+                        <div class="text-center">
+                            <button class="btn btn-info" type="submit">Log in</button>
+                            <span class="help-block">
+                                <?php echo $error;?>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
+        </div>
+    </body>
