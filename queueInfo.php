4eb2e7ab57847c921fb8c4df0b7e5a46b74451de
Koder <6042@ait.nsw.edu.au>
2017-08-14 11:57:54 +0000

    ticket page updated

--- queueInfo.php
+++ queueInfo.php
@@ -81,16 +81,7 @@
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" http-equiv="refresh" content="5">
-    <title><?php echo $page_title;?></title>
-    
-    <link rel="stylesheet" href="components/bootstrap/dist/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="includes/mystyle.css">
-    
-    <script src="components/jquery/dist/jquery.js"></script>
-    <script src="components/bootstrap/dist/js/bootstrap.js"></script>
-</head>
+  <?php include("includes/head.php");?>
   
   <body>
     <div class="header">
--- totem.php
+++ totem.php
@@ -4,13 +4,22 @@ session_start();
 
 $_SESSION["queueType"] = $_POST['inlineRadioOptions'];
 
-$sqlaccess = mysqli_query($connection, "SELECT * FROM QUEUE WHERE ID = (SELECT MAX(ID) FROM QUEUE)");
-$row = mysqli_fetch_assoc($sqlaccess);
+$sqlquery = mysqli_query($connection, "SELECT * FROM QUEUE WHERE ID = (SELECT MAX(ID) FROM QUEUE)");
+$row = mysqli_fetch_assoc($sqlquery);
 
 $ticketCode = $row['ACCESS_CODE'];
 $ticketId      = $row['ID'];
 $ticketCreated = $row['CREATED'];
 $ticketQueueId = $row['QUEUE_ID'];
+
+$sqlquery = mysqli_query($connection, "SELECT * FROM queue_type");
+
+if($sqlquery->num_rows>0){
+    $queuenames = array();
+    while($row = $sqlquery->fetch_assoc()){
+        array_push($queuenames, $row);
+    }
+}
 
 if(isset($_SESSION["queueType"])){
    header("location: totemTicket.php");
@@ -23,30 +32,70 @@ else{
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Next Queue Info</title>
-        <link href="style.css" rel="stylesheet" type="text/css">
-    </head>
+    <?php include("includes/head.php");?>
     <body>
-        <center><div id="profile">
-
-        <form action="" method="post">
-            <label class="radio-inline">
-                  <input type="radio" name="inlineRadioOptions" value="A"> A
-            </label>
-            <label class="radio-inline">
-                  <input type="radio" name="inlineRadioOptions" value="B"> B
-            </label>
-            <label class="radio-inline">
-                  <input type="radio" name="inlineRadioOptions" value="C"> C
-            </label><br /><br />
-            <b><input name="submit" type="submit" value=" Get Ticket "></b><br /><br />
-        </form>
-        <b>Your Access code: </b><?php echo $ticketCode; ?><br />
-        <b>Your Position: </b><?php echo $ticketId; ?><br />
-        <b>Ticket obtained at: </b><?php echo $ticketCreated; ?><br />
-        <b>QueueID: </b><?php echo $ticketId, $ticketQueueId; ?><br />
-        </div></center>
+        
+            <div class="totemheader">
+                <center>
+                    <img class="totemlogo" src="images/logo.png">
+                </center>
+            </div>
+            <div class="container">
+                <div class="row rowcenter">
+                    <div class="col-lg-12">
+                    <form action="" method="post">
+                    <?php
+                        if(count($queuenames)>0){
+                            $counter = 0;
+                            $total = count($queuenames);
+                            
+                            foreach($queuenames as $qname){
+                                $name = $qname["queue_type"];
+                                $id = $qname["queue_id"];
+                                
+                                
+                                if($counter==3){
+                                    echo "</div>";
+                                    $counter = 0;
+                                }
+                                if($counter==0){
+                                    echo "<div class=\"row\">";
+                                }
+                                echo "<div class=\"col-lg-4\">
+                                        <label class=\"radio-inline\">
+                                            <input type=\"radio\" name=\"inlineRadioOptions\" value=$id> $name
+                                        </label>
+                                    </div>";
+                                $counter++;
+                            }
+                        }
+                        
+                        ?>
+                </div>
+                <div class="row rowcenterbutton">
+                    <b><input name="submit" class="btn btn-primary" type="submit" value=" Get Ticket "></b><br /><br />
+                </div>
+                </form>
+            </div>
+            
+        </div>
     </body>
 </html>
-
+            <!--<form action="" method="post">
+                    <label class="radio-inline">
+                          <input type="radio" name="inlineRadioOptions" value="A"> A
+                    </label>
+                    <label class="radio-inline">
+                          <input type="radio" name="inlineRadioOptions" value="B"> B
+                    </label>
+                    <label class="radio-inline">
+                          <input type="radio" name="inlineRadioOptions" value="C"> C
+                    </label><br /><br />
+                    <b><input name="submit" class="btn btn-primary" type="submit" value=" Get Ticket "></b><br /><br />
+                </form>
+                <div>
+                <b>Your Access code: </b><?php echo $ticketCode; ?><br />
+                <b>Your Position: </b><?php echo $ticketId; ?><br />
+                <b>Ticket obtained at: </b><?php echo $ticketCreated; ?><br />
+                <b>QueueID: </b><?php echo $ticketId, $ticketQueueId; ?><br />
+            </div>-->
