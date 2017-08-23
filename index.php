435f37bb16a8f7e88a86765624f706827386fb35
Koder <6042@ait.nsw.edu.au>
2017-08-23 01:38:19 +0000

    check

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if(isset($_SESSION['login_user'])){
 <html>
     <head>
         <title>Next Queue Tracker</title>
-        <link href="style.css" rel="stylesheet" type="text/css">
+        <link href="includes/mystyle.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div id="main">
